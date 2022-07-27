f6965dd6e348e7337e885e4407d518a8e57e33c1
藤井真 <fuzimako0202@gmail.com>
2022-07-27 23:19:06 +0900

    投稿ページを作成 CreatePost.tsxの中にHTMLをあて、投稿ページの大枠を作成した

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,6 +1,22 @@
 
 const CreatePost:any = () => {
-    return <div>メイン</div>;
+    return (
+    <div className="createPostPage">
+        <div className="postContainer">
+            <h1>記事を投稿する</h1>
+            <div className="inputPost">
+                <div>タイトル</div>
+                <input type="text" placeholder="タイトルを記入" />
+            </div>
+            <div className="inputPost">
+                <div>投稿</div>
+                <textarea placeholder="投稿内容を記入"></textarea>
+            </div>
+            <button className="postButton">投稿する</button>
+
+        </div>
+    </div>
+    )
 };
 
 export default CreatePost;
