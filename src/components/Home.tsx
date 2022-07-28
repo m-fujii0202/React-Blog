29bd26275d38c9daa8cb98929986c1d82f5cffad
藤井真 <fuzimako0202@gmail.com>
2022-07-28 23:04:53 +0900

    HomePage作成の枠作成しました

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,20 @@
 
 const Home:any = () => {
     return (
-    <div>ホーム</div>
+    <div className="homePage">
+        <div className="postContents">
+            <div className="postHeader">
+                <h1>タイトル</h1>
+            </div>
+        </div>
+            <div className="postTextContainer">
+                React学集中です!
+            </div>
+            <div className="nameAndDeleteButton">
+               <h3>@makoto</h3>
+               <button>削除</button>
+            </div>
+    </div>
     )
 };
 
