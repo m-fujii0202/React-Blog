f746fb25a8031230da321b9f9133804b827943a6
Tatsuya Fujii <kassi081dm@gmail.com>
2022-07-28 13:57:09 +0900

    ログイン時のみ記事投稿ページに遷移できるように変更

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -4,104 +4,101 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { auth, db } from "../firebase";
 
-
 const CreatePost = () => {
-    const [title, setTitle] = useState<string>();
-    const [postText, setPostTitle] = useState<string>();
+  const [title, setTitle] = useState<string>();
+  const [postText, setPostTitle] = useState<string>();
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const createPost = async () => {
-        //     console.log(title);
-        //     console.log(postText);
-        await addDoc(collection(db, "posts"), {
-            title: title,
-            postsText: postText,
-            author: {
-                username: auth.currentUser?.displayName,
-                id: auth.currentUser?.uid,
-            },
-        });
-        navigate("/");
-    };
+  const createPost = async () => {
+    //     console.log(title);
+    //     console.log(postText);
+    await addDoc(collection(db, "posts"), {
+      title: title,
+      postsText: postText,
+      author: {
+        username: auth.currentUser?.displayName,
+        id: auth.currentUser?.uid,
+      },
+    });
+    navigate("/");
+  };
 
-    return (
-        <ScreatePostPage>
-            <SpostContainer>
-                <Sh1>記事を投稿する</Sh1>
-                <div className="inputPost">
-                    <div>タイトル</div>
-                    <Sinput
-                        type="text"
-                        placeholder="タイトルを記入"
-                        onChange={(e) => setTitle(e.target.value)}
-                    />
-                </div>
-                <div className="inputPost">
-                    <div>投稿</div>
-                    <Stextarea
-                        placeholder="投稿内容を記入"
-                        onChange={(e) => setPostTitle(e.target.value)}
-                    ></Stextarea>
-                </div>
-                <Sbutton className="postButton" onClick={createPost}>投稿する</Sbutton>
-            </SpostContainer>
-        </ScreatePostPage>
-    )
+  return (
+    <ScreatePostPage>
+      <SpostContainer>
+        <Sh1>記事を投稿する</Sh1>
+        <div className="inputPost">
+          <div>タイトル</div>
+          <Sinput
+            type="text"
+            placeholder="タイトルを記入"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="inputPost">
+          <div>投稿</div>
+          <Stextarea
+            placeholder="投稿内容を記入"
+            onChange={(e) => setPostTitle(e.target.value)}
+          ></Stextarea>
+        </div>
+        <Sbutton className="postButton" onClick={createPost}>
+          投稿する
+        </Sbutton>
+      </SpostContainer>
+    </ScreatePostPage>
+  );
 };
 
 const ScreatePostPage = styled.div`
- display:flex;
- align-items:center;
- justify-content:center;
- height:90vh;
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 90vh;
+`;
 
 const SpostContainer = styled.div`
- background-color:#fff;
- border-radius:10px;
- display:flex;
- flex-direction:column;
- justify-content:center;
- height:400px;
- width:500px;
- gap:10px;
- padding:0 40px;
- box-shadow: 12px 14px 5px 13px #777777;
- `
+  background-color: #fff;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 400px;
+  width: 500px;
+  gap: 10px;
+  padding: 0 40px;
+  box-shadow: 12px 14px 5px 13px #777777;
+`;
 
 const Sh1 = styled.h1`
- margin:0;
- `
-
+  margin: 0;
+`;
 
 const Sinput = styled.input`
-    width:100%;
-    height:30px;
-    outline:none;
-    padding:0 4px;
-`
-
-
+  width: 100%;
+  height: 30px;
+  outline: none;
+  padding: 0 4px;
+`;
 
 const Stextarea = styled.textarea`
-    width:100%;
-    height:120px;
-    outline:none;
-    padding:7px;
-`
+  width: 100%;
+  height: 120px;
+  outline: none;
+  padding: 7px;
+`;
 
 const Sbutton = styled.button`
-    padding:10px 0;
-    background-color:#3d77a5;
-    color:#fff;
-    border-radius:5px;
-    cursor:pointer;
-    transition: all 0.3s;
-    &:hover{
-        transform:translateY(5px);
-    }
-`
-
+  padding: 10px 0;
+  background-color: #3d77a5;
+  color: #fff;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: all 0.3s;
+  &:hover {
+    transform: translateY(5px);
+  }
+`;
 
 export default CreatePost;
--- src/components/Navber.tsx
+++ src/components/Navber.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHouse,
   faFilePen,
-  faArrowRightToBracket
-} from '@fortawesome/free-solid-svg-icons'
+  faArrowRightToBracket,
+} from "@fortawesome/free-solid-svg-icons";
 
 type PropsType = {
   isAuth: boolean;
-}
-
+};
 
 export const Navber = (props: PropsType) => {
   const { isAuth } = props;
@@ -22,46 +21,45 @@ export const Navber = (props: PropsType) => {
         ホーム
       </NavLink>
 
-      <NavLink to="/createpost">
-        <FontAwesomeIcon icon={faFilePen} />
-        記事投稿
-      </NavLink>
-
       {!isAuth ? (
         <NavLink to="/login">
           <FontAwesomeIcon icon={faArrowRightToBracket} />
           ログイン
         </NavLink>
       ) : (
-        <NavLink to="/logout">
-          <FontAwesomeIcon icon={faArrowRightToBracket} />
-          ログアウト
-        </NavLink>
+        <>
+          <NavLink to="/createpost">
+            <FontAwesomeIcon icon={faFilePen} />
+            記事投稿
+          </NavLink>
+
+          <NavLink to="/logout">
+            <FontAwesomeIcon icon={faArrowRightToBracket} />
+            ログアウト
+          </NavLink>
+        </>
       )}
-
-
     </Snav>
-  )
+  );
 };
 
 const Snav = styled.div`
- display:flex;
- justify-content:center;
- align-items:center;
- height:50px;
- background-color:#cb8c54;
- gap:45px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
+  background-color: #cb8c54;
+  gap: 45px;
 `;
 
 const NavLink = styled(Link)`
-    text-decoration: none;
-    color:#fff;
-    font-size: 20px;
-    margin: 5px 0;
-    transition:all 0.3s;
-    &:hover {
-        opacity: 0.7;
-        color: #000;
-    }
+  text-decoration: none;
+  color: #fff;
+  font-size: 20px;
+  margin: 5px 0;
+  transition: all 0.3s;
+  &:hover {
+    opacity: 0.7;
+    color: #000;
+  }
 `;
-
